08bf5bc4e50c201093d65232d017c2878d258168
Eckhardt-D <eckhardt.dreyer@gmail.com>
2023-05-03 06:58:51 +0200

    fix: remove nuxt test for stackblitz use fs

--- 1-nuxi/__tests__/main.test.ts
+++ 1-nuxi/__tests__/main.test.ts
@@ -1,12 +1,7 @@
 import { describe, test, expect } from "vitest";
-import { setup, $fetch } from "@nuxt/test-utils";
 import { readFileSync } from "fs";
 
 describe("nuxi command tests", async () => {
-  await setup({
-    configFile: "./nuxt.config.ts",
-  });
-
   test("dev script modified", () => {
     const pkg = readFileSync("./package.json", "utf-8");
     const js = JSON.parse(pkg);
@@ -14,8 +9,9 @@ describe("nuxi command tests", async () => {
   });
 
   test("nuxi generated files", async () => {
-    const response = await $fetch("/");
-    expect(response).toContain("<h1>Hello");
-    expect(response).toContain("<button>Click");
+    const page = readFileSync("./pages/index.vue", "utf-8");
+    const component = readFileSync("./components/Button.vue", "utf-8");
+    expect(page).toContain("<h1>Hello");
+    expect(component).toContain("<button>Click");
   });
 });
